a4305207362fbf728b9f5a15758e1cfd2bac34e2
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2018-07-30 16:06:13 +0200

    implemented e4 example
    https://dirksmetric.wordpress.com/2012/08/01/tutorial-eclipse-rcp-e4-with-3-x-views-like-project-explorer-properties-etc/

--- bundles/net.tourbook.e4/src/net/tourbook/e4/ApplicationWorkbenchWindowAdvisor.java
+++ bundles/net.tourbook.e4/src/net/tourbook/e4/ApplicationWorkbenchWindowAdvisor.java
@@ -8,21 +8,24 @@ import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
 public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 
-    public ApplicationWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
-        super(configurer);
-    }
-    
-    @Override
-    public ActionBarAdvisor createActionBarAdvisor(IActionBarConfigurer configurer) {
-        return new ApplicationActionBarAdvisor(configurer);
-    }
-    
-    @Override
-    public void preWindowOpen() {
-        IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
-        configurer.setInitialSize(new Point(400, 300));
-        configurer.setShowCoolBar(false);
-        configurer.setShowStatusLine(false);
-        configurer.setTitle("Hello RCP"); //$NON-NLS-1$
-    }
+	public ApplicationWorkbenchWindowAdvisor(final IWorkbenchWindowConfigurer configurer) {
+		super(configurer);
+	}
+
+	@Override
+	public ActionBarAdvisor createActionBarAdvisor(final IActionBarConfigurer configurer) {
+		return new ApplicationActionBarAdvisor(configurer);
+	}
+
+	@Override
+	public void preWindowOpen() {
+
+		final IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+
+		configurer.setInitialSize(new Point(400, 300));
+		configurer.setShowCoolBar(true);
+		configurer.setShowStatusLine(true);
+
+		configurer.setTitle("MyTourbook E4"); //$NON-NLS-1$
+	}
 }
